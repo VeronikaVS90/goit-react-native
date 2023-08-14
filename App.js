9a888c5c7d28b536a58ac69068188e8f00cde77e
VeronikaVS90 <vsokolovanika@gmail.com>
2023-08-15 01:42:23 +0300

    fix

--- App.js
+++ App.js
@@ -1,15 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-// import LoginScreen from './src/Screens/AuthPages/LoginScreen/LoginScreen';
-import RegistrationScreen from './src/Screens/AuthPages/RegistrationScreen/RegistrationScreen';
+import RegistrationScreen from './src/screens/RegistrationScreen';
+import LoginScreen from './src/screens/LoginScreen';
+import { useFonts } from "expo-font";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf"),
+    "Roboto-Bold": require("./assets/fonts/Roboto-Bold.ttf"),
+    "Roboto-Medium": require("./assets/fonts/Roboto-Medium.ttf"),
+  });
+
+  if(!fontsLoaded) {
+    return null;
+  };
+
   return (
     <View style={styles.container}>
-      {/* <LoginScreen /> */}
-      <RegistrationScreen />
-      {/* <Text>Open up App.js to start working on your app!</Text> */}
-      <StatusBar style="auto" />
+      {/* <RegistrationScreen /> */}
+      <LoginScreen />
     </View>
   );
 }
@@ -17,8 +25,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    position: 'relative'
   },
 });
