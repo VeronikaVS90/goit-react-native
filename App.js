72fafde51cd1163a1df282d69039918382623365
VeronikaVS90 <vsokolovanika@gmail.com>
2023-08-18 00:46:20 +0300

    Fixed bugs and did the 3rd task

--- App.js
+++ App.js
@@ -16,8 +16,8 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      {/* <RegistrationScreen /> */}
-      <LoginScreen />
+      <RegistrationScreen />
+      {/* <LoginScreen /> */}
     </View>
   );
 }
